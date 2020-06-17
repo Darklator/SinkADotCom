65f30fe87464110dcea0f4e15b7e2b9902f2b56b
Dmitry <dimaion666@gmail.com>
2020-06-17 13:32:10 +0500

    Java

---
+++ DotComBust.java
@@ -0,0 +1,72 @@
+import java.util.*;
+public class DotComBust {
+		// Объявляем и инициализируем переменные, которые нам понадобятся.	
+		private GameHelper helper = new GameHelper();
+		private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+		private int numOfGuesses = 0;
+
+		// Создаём три объекта DotCom, даём им имена и помещаем в ArrayList.
+		private void setUpGame() {
+			DotCom one = new DotCom();
+			one.setName("Pets.com");
+			DotCom two = new DotCom();
+			two.setName("eToys.com");
+			DotCom three = new DotCom();
+			three.setName("Go2.com");
+			dotComsList.add(one);
+			dotComsList.add(two);
+			dotComsList.add(three);
+
+		// Выводим краткие инструкции для пользователя.		
+			System.out.println("Ваша цель - потопить три сайта.");
+			System.out.println("Pets.com, eToys.com, Go2.com");
+			System.out.println("Попытайтесь потопить их за минимальное количество ходов");
+		
+			for (DotCom dotComToSet : dotComsList) { // Повторяем с каждым объектом DotCom в списке.
+				ArrayList<String> newLocation = helper.placeDotCom(3); // Запрашиваем у вспомогательного объекта адрес сайта
+				dotComToSet.setLocationCells(newLocation); // Вызываем сеттер из объекта DotCom, чтобы передать ему местоположение, которое только что получили от вспомогательного объекта.
+			}
+		}
+		
+		private void startPlaying() {
+			while(!dotComsList.isEmpty()) { // До тех пор, пока список объектов не станет пустым.
+				String userGuess = helper.getUserInput("Сделайте ход"); // Получаем пользовательский ввод.
+				checkUserGuess(userGuess); // Вызываем наш метод checkUserGuess
+			}
+			finishGame(); // Вызываем наш метод finishGame
+		}
+		
+		private void checkUserGuess(String userGuess) {
+			numOfGuesses++; // iнкрементируем количество попыток, которые сделал пользователь.
+			String result = "Мимо"; // Подразумеваем промах, пока не выяснили обратного.
+			
+			
+			for (DotCom dotComToTest : dotComsList) { // Повторяем это для всех объектов DotCom в списке.
+				result = dotComToTest.checkYourself(userGuess); // Просим DotCom проверить ход пользователя, ищем прпадание(или потопление).
+				
+				if (result.equals("Попал") || result.equals("Потопил")) {
+					break; // Выбираемся из цикла раньше времени, нет смысла проверять другие сайты.
+				}
+			}
+			System.out.println(result); // Выводим для пользователя результат.
+		}
+		
+		private void finishGame() {
+		// Выводим сообщение о том, как пользователь прошёл игру.			
+			System.out.println("Все сайты ушли ко дну! Ваши акции теперь ничего не стоят.");
+			if (numOfGuesses <= 18) {
+				System.out.println("Это заняло у вас всего" + numOfGuesses + "попыток.");
+				System.out.println("Вы успели выбраться до того, как ваши вложения утонули.");
+			} else {
+				System.out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
+				System.out.println("Рыбы водят хороводы вокруг ваших вложений.");
+			}
+		}
+		
+		public static void main (String[] atgs) {
+			DotComBust game = new DotComBust(); // Создаём игровой объект.
+			game.setUpGame(); // Говорим игровому объекту подготовить игру.
+			game.startPlaying(); // Говорим игровому объекту начать главный игровой цикл (продолжаем запрашивать пользовательский ввод и проверять полученные данные).
+		}
+}
+		
